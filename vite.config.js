9ec524633405f088df7295f2e9011fde4ff6483d
Jorge <jorgefreire2403@hotmail.com>
2025-12-02 22:40:40 -0300

    Cambio

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,3 @@ export default defineConfig(({ command }) => ({
   plugins: [react()],
   base: command === 'serve' ? '/' : '/Portafolio/',
 }))
-<<<<<<< HEAD
-=======
-
->>>>>>> 3e0a88ff790190e02b3d6009023311c9f20eb29a
