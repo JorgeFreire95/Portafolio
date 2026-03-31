bf7cb7748c4b36844ad74fe7c60f42b071ec34fb
Jorge Freire <jorgefreire2403@gmail.com>
2026-03-31 13:59:16 -0300

    Cambio

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -9,6 +9,13 @@ const Experience = () => {
             company: 'Crazy Family',
             period: '2025',
             description: 'Desarrollo de servicios backend en Node.js y Express para sincronización de stock y precios. Implementación de procesos de carga masiva de productos, reduciendo en un 40% los tiempos operativos. Validación y control de datos para asegurar la integridad y consistencia de la información. Trabajo con APIs REST y bases de datos relacionales',
+        },
+        {
+            id: 2,
+            role: 'Desarrollador FullStack',
+            company: 'Freelance',
+            period: '2026',
+            description: 'Desarrollé una aplicación móvil orientada a empresas de seguridad, enfocada en la gestión y supervisión de rondas operativas. La solución permite la marcación de puntos de control en tiempo real, asegurando el cumplimiento de recorridos establecidos por el personal de vigilancia. Además, implementé un sistema de registro de asistencia integrado, que facilita el control de horarios y la trazabilidad de los guardias. La aplicación mejora la eficiencia operativa, la supervisión remota y la generación de reportes para la toma de decisiones.\n\nFunciones principales:\n\n-Registro y seguimiento de rondas mediante marcación de puntos de control\n-Sistema de control de asistencia del personal\n-Monitoreo en tiempo real de actividades\n-Generación de reportes operativos\n-Mejora en la trazabilidad y cumplimiento de protocolos de seguridad',
         }
     ];
 
